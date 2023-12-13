eaae3d6dcc87b1f2a267461acc7e0a56a0c4206a
elyorbekvich <diyorbektursunboyev000@gmail.com>
2023-12-13 20:47:10 +0500

    java commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var salom = alert('Assalomu alaykum')
+var isim = prompt('Iltimos,ismingizni kiriting')
+console.log(isim);
+var tel = prompt("Telefon Nomeringizni kiriting")
+console.log(tel);
+var yosh = prompt('Yoshingizni kiriting')
+console.log(yosh);
